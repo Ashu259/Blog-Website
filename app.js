b1243e472981d960d40a27a314e02cd2f918192c
Ashu259 <agrawalashutosh755@gmail.com>
2022-11-20 23:16:12 +0530

    Updated mongodb url

--- app.js
+++ app.js
@@ -4,20 +4,14 @@ const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
-const mongoose = require("mongoose");
 
 const app = express();
 
-// require('dotenv').config();
+require('dotenv').config();
 
 app.use(express.urlencoded( {extended: true} ));
 app.use(express.static('public'));
 app.use(expressLayouts);
-
-main().catch(err => console.log(err));
-async function main() {
-  await mongoose.connect("mongodb+srv://admin-ashu:pZBeBTT9FE7K2yu0@cluster0.ihw6yn6.mongodb.net/blogprojectDB");
-}
 
 app.use(cookieParser('BlogSecure'));
 app.use(session({
